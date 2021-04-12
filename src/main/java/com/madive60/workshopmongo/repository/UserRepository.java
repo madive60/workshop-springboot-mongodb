cdfc4d0d7bebcc0c687081e44c8e63b3854f4ee1
madive60 <madive60@gmail.com>
2021-04-12 16:30:08 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/madive60/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.madive60.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.madive60.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
