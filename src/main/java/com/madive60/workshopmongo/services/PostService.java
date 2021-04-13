fa567fe7fa56bf979449f033e5332617ea254855
madive60 <madive60@gmail.com>
2021-04-13 16:10:27 -0300

    Obtendo um post por id

---
+++ src/main/java/com/madive60/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.madive60.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.madive60.workshopmongo.domain.Post;
+import com.madive60.workshopmongo.repository.PostRepository;
+import com.madive60.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
